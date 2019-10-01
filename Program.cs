c9c3c5d9a0b56a89e8fb9bcb478458f9ef83b736
Tariq <tariq.hameed@utb.ecutbildning.se>
2019-10-01 13:08:26 +0200

    connected with database

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using static System.Console;
+using System.Threading;
+using Microsoft.Data.SqlClient;
 
 namespace Parkeringhus
 {
     class Program
     {
+        static string connenctionstring = "Data Source = .; Initial Catalog = Parkeringhus; Integrated Security = true";
         static void Main(string[] args)
         {
 
@@ -26,6 +29,48 @@ namespace Parkeringhus
                 {
                     case ConsoleKey.D1:
                     case ConsoleKey.NumPad1:
+                        Write("Customer Name:");
+                        string customer = ReadLine();
+
+
+                        Write("contact details:");
+                        string contactDetails = ReadLine();
+
+                        Write("Registration number:");
+                        string registrationNumber = ReadLine();
+
+                        Write("Description:");
+                        string description = ReadLine();
+                        DateTime arrival = DateTime.Now;
+                        string query = @"INSERT INTO Parking(Customer, ContactDetails, RegistrationNumber, Description, Arrival)
+                                        VALUES(@Customer, @ContactDetails, @RegistrationNumber, @Description, @Arrival)";
+
+                        SqlConnection connection = new SqlConnection(connenctionstring);
+                        SqlCommand command = new SqlCommand(query, connection);
+
+                        command.Parameters.AddWithValue("@Customer", customer);
+                        command.Parameters.AddWithValue("@ContactDetails", contactDetails);
+                        command.Parameters.AddWithValue("@RegistrationNumber", registrationNumber);
+                        command.Parameters.AddWithValue("@Description", description);
+                        command.Parameters.AddWithValue("@Arrival", arrival);
+
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                        connection.Close();
+
+                        // Add method to insert record into database
+
+                        Clear();
+
+                        WriteLine("Parkering registered");
+
+                        Thread.Sleep(2000);
+                        
+                        //Write("Arrival (yyyy-mm-dd  hh:mm):");
+                        //string arrival = ReadLine();
+
+                        //Write("Departure (yyyy-mm-dd  hh:mm):");
+                        //string departure = ReadLine();
 
                         break;
 
