f170f23d3740ada21677e8889bb5c890cc8c39e6
Tariq <tariq.hameed@utb.ecutbildning.se>
2019-10-01 10:04:46 +0200

    Add menu and exit ability

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace Parkeringhus
 {
@@ -6,7 +7,51 @@ namespace Parkeringhus
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            bool shouldNotExit = true;
+            while (shouldNotExit)
+
+            {
+
+                Console.WriteLine("1. Register arrivals");
+                Console.WriteLine("2. Register departure");
+                Console.WriteLine("3. Show Parking registery");
+                Console.WriteLine("4. Exit");
+
+                ConsoleKeyInfo keypressed = ReadKey(true);
+                Clear();
+
+                switch (keypressed.Key)
+                {
+                    case ConsoleKey.D1:
+                    case ConsoleKey.NumPad1:
+
+                        break;
+
+                    case ConsoleKey.D2:
+                    case ConsoleKey.NumPad2:
+
+                        break;
+
+                    case ConsoleKey.D3:
+                    case ConsoleKey.NumPad3:
+
+                        break;
+
+                    case ConsoleKey.D4:
+                    case ConsoleKey.NumPad4:
+                        shouldNotExit = false;
+                        break;
+
+
+
+                }
+
+
+                Clear();
+
+            }
         }
     }
 }
